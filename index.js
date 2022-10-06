8459243688ef06905fed470a81ae4ed2cedfccb6
Emily Hill <emhillmed2021@gmail.com>
2022-10-06 13:14:30 -0500

    started prompts to fill out README, changed to dsingle quote ('') because double quote () need around prompt words, will us from now on

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 // TODO: Include packages needed for this application
 
 const inquirer = require("inquirer");
-
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
+const promptQuestions = () => {
+
 const questions = [];
 
+if (!questions) {
+    questions = []; 
+}
+return inquirer 
+.prompt([
+    {
+        type: 'input', 
+        name: 'name',
+        message: 'What is the title of the project? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true; 
+            } else {
+                console.log('Please enter a project title with the following format: #<project-title>')
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of the project (Required).',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a project description.')
+                return false; 
+            }
+        }
+    },
+    {},
+])
+}
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
