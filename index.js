47e17a1f81b81ad17d0b951e4097f5782d9da176
Emily Hill <emhillmed2021@gmail.com>
2022-10-06 12:49:53 -0500

    a commit just to make sure everything is on the same page

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // TODO: Include packages needed for this application
 
 const inquirer = require("inquirer");
-console.log(inquirer)
-const fs = require("fs"); 
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
