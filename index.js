5bcda12f0c5795c146b962bbdb79e591fb2096fe
Emily Hill <emhillmed2021@gmail.com>
2022-10-06 13:36:48 -0500

    finished bare minimun questions, might add more with ability to format screenshot and image links

--- index.js
+++ index.js
@@ -39,9 +39,61 @@ return inquirer
             }
         }
     },
-    {},
+    {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like the README to contain a table of contents?',
+        default: false
+    },
+    { 
+        type: 'checkbox',
+        name: 'tableOfContents',
+        message: 'Which sections would you like to add to the table of contents?',
+        choices: ['[Installation](#installation)', '[Usage](#usage)', '[Credits](#credits)', '[License](#license)']
+    },
+    { 
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use. Include screenshots as needed (Required).',
+        validate: nameInput => {
+            if (nameInput) {
+                return true
+            }else{
+                console.log('Please provide instructions and examples for use.')
+                return false
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Provide names of any collaborators, third-party assets that require attribution, or any consulted tutorials.'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Please link your license here (Required).',
+        validate: nameInput => {
+            if (nameInput) {
+                return true
+            } else {
+                console.log('Please enter a link to license.')
+                return false 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'howToContribute',
+        message: 'If your app or package allows for contributors, include the guidlines for how they may do so.'
+    },
 ])
-}
+};
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
