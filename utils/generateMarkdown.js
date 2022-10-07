dead91a530b64febbbe89efa0e67a73d31fe0869
Emily Hill <emhillmed2021@gmail.com>
2022-10-07 00:41:33 -0500

    maybe this will work for badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -19,15 +19,14 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title} 
+  ![license]('https://img.shields.io/badge/license-${data.license}-ff69b4')
   ## ${data.description}
   ## ${data.tableOfContents}
   ## ${data.installation}
   ## ${data.usage}
   ## ${data.credits}
   ## ${data.howToContribute}
-  ## ${data.license}
-  ${renderLicenseBadge}
-   ${renderLicenseLink} `
+  ## ${data.license} `
 }
 
 module.exports = generateMarkdown;
